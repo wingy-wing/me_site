b6960b578080b3cfdfa22aa146eadf3a7bcae60d
Alex <alex.wingard92@gmail.com>
2016-09-06 20:08:55 +0100

    First Commit

---
+++ www/js/controllers/accordionToggle.js
@@ -0,0 +1,12 @@
+/**
+ * Created by alex on 03/09/16.
+ */
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+    }
+}
